2bcb2ced877899031139388b52e9dae5fb8cd81c
Sam J Hill <samuhill@gmail.com>
2018-02-26 14:43:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,3 @@
 docker exec -it lightning bash
 
-cd home
-
-git clone https://github.com/samjhill/node-lightning-payments
-
-cd node-lightning-payments
-
-curl -sL https://deb.nodesource.com/setup_8.x | bash
-
-apt-get install nodejs -y
-
-apt-get install npm -y
-
-npm install forever -g
-
-npm install
-
-forever start index.js
+cd home && git clone https://github.com/samjhill/node-lightning-payments && cd node-lightning-payments && curl -sL https://deb.nodesource.com/setup_8.x | bash && apt-get install nodejs -y && apt-get install npm -y && npm install forever -g && npm install && forever start index.js
